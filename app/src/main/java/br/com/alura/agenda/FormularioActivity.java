fe40f966acf5ac91116a4db60f9e88704ed07343
ericvyc <eric_vilar2007@hotmail.com>
2016-07-13 18:53:39 -0300

    Listener de clique do botão.

--- app/src/main/java/br/com/alura/agenda/FormularioActivity.java
+++ app/src/main/java/br/com/alura/agenda/FormularioActivity.java
@@ -3,6 +3,9 @@ package br.com.alura.agenda;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class FormularioActivity extends AppCompatActivity {
 
@@ -12,6 +15,15 @@ public class FormularioActivity extends AppCompatActivity {
         setContentView(R.layout.activity_formulario);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Button botaoSalvar = (Button) findViewById(R.id.formulario_salvar);
+        botaoSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(FormularioActivity.this, "Botão clicado!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
 }
