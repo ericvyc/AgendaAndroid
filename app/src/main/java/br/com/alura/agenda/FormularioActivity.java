8855c060a7c7835d8da3ead67027d44f91ee0628
ericvyc <eric_vilar2007@hotmail.com>
2016-07-18 19:59:47 -0300

    Comentários no código.

--- app/src/main/java/br/com/alura/agenda/FormularioActivity.java
+++ app/src/main/java/br/com/alura/agenda/FormularioActivity.java
@@ -23,12 +23,17 @@ public class FormularioActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //Instancia FormularioHelper
         helper = new FormularioHelper(this);
 
+        //Pega intent caso tenha sido colocado ao chamar a Activity (Usado na edição)
         Intent intent = getIntent();
+        //Seta aluno que foi passado na Intent
         Aluno aluno = (Aluno) intent.getSerializableExtra("aluno");
 
+        //Testa se o aluno é nulo (Usado na edição)
         if(aluno != null) {
+            //Se o aluno não for nulo, instancia seus valores nos campos do formulario
             helper.preencheFormulario(aluno);
         }
 
@@ -36,8 +41,9 @@ public class FormularioActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        //Instancia menu no cabeçalho
         MenuInflater inflater = getMenuInflater();
-
+        //Pega XML e coloca como layout
         inflater.inflate(R.menu.menu_formulario, menu);
 
         return super.onCreateOptionsMenu(menu);
@@ -46,22 +52,31 @@ public class FormularioActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
+        //Switch para saber qual opção do menu foi clicado
         switch (item.getItemId()) {
             case R.id.menu_formulario_ok:
-
+                //Se a opção clicada for a opção salvar chama o helper para instanciar o aluno
+                // pelos campos do formulario
                 Aluno aluno = helper.pegaAluno();
+                //Instancia DAO
                 AlunoDAO dao = new AlunoDAO(this);
 
+                //Testa se o id do aluno é nulo ou não
                 if(aluno.getId() != null) {
+                    //Se o id não for nulo, chama o metodo de alteração
                     dao.altera(aluno);
                 } else {
+                    //Se o id for nulo, cria um novo aluno
                     dao.insere(aluno);
                 }
 
+                //Fecha o DAO
                 dao.close();
 
+                //Chama o Toast com o nome do aluno que foi salvo
                 Toast.makeText(FormularioActivity.this, "Aluno " + aluno.getNome() + " salvo!", Toast.LENGTH_SHORT).show();
 
+                //Termina Activity
                 finish();
                 break;
         }
--- app/src/main/java/br/com/alura/agenda/FormularioHelper.java
+++ app/src/main/java/br/com/alura/agenda/FormularioHelper.java
@@ -10,27 +10,33 @@ import br.com.alura.agenda.modelo.Aluno;
  */
 public class FormularioHelper {
 
+    //Criando os campos do formulario como atributo da classe
     private final EditText campoNome;
     private final EditText campoEndereco;
     private final EditText campoTelefone;
     private final EditText campoSite;
     private final RatingBar campoNota;
 
+    //Cria atributo aluno
     private Aluno aluno;
 
     public FormularioHelper(FormularioActivity activity) {
 
+        //Recebe formulario como parametro e instancia os campos locais como
+        // atributos da classe atraves do formulario
         campoNome = (EditText) activity.findViewById(R.id.formulario_nome);
         campoEndereco = (EditText) activity.findViewById(R.id.fomrulario_endereco);
         campoTelefone = (EditText) activity.findViewById(R.id.formulario_telefone);
         campoSite = (EditText) activity.findViewById(R.id.formulario_site);
         campoNota = (RatingBar) activity.findViewById(R.id.formulario_nota);
+        //Instancia aluno
         aluno = new Aluno();
 
     }
 
     public Aluno pegaAluno() {
 
+        //Seta valores do formulario nos atributos do aluno
         aluno.setNome(campoNome.getText().toString());
         aluno.setEndereco(campoEndereco.getText().toString());
         aluno.setTelefone(campoTelefone.getText().toString());
@@ -41,6 +47,10 @@ public class FormularioHelper {
     }
 
     public void preencheFormulario(Aluno aluno) {
+
+        //Pega os valores do aluno passado como parâmetro, instancia nos campos
+        // do formulario e instancia o aluno passado no aluno local
+        // (Para usar na edição em outra ação)
         campoNome.setText(aluno.getNome());
         campoEndereco.setText(aluno.getEndereco());
         campoTelefone.setText(aluno.getTelefone());
