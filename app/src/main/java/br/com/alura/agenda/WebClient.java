8bc5b77c272c85d34b98e7c156e4944642174a9b
ericvyc <eric_vilar2007@hotmail.com>
2016-07-20 20:31:01 -0300

    Adequação de comentários no código.

--- app/src/main/java/br/com/alura/agenda/WebClient.java
+++ app/src/main/java/br/com/alura/agenda/WebClient.java
@@ -21,23 +21,25 @@ public class WebClient {
             //Seta a conexão da url fazendo cast para HttpURLConnection
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
-            //Informa na requisição que o formato dos dados enviados é json
+            //Informa na requisição que o formato dos dados enviados é JSON
             connection.setRequestProperty("Content-type", "application/json");
 
-            //Informa na requisição que a resposta tem que ser em formato json
+            //Informa na requisição que a resposta tem que ser em formato JSON
             connection.setRequestProperty("Accept", "application/json");
 
-            //Seta na conexão a inteção de ler a resposta (POST)
+            //Seta na conexão que os dados estarão no corpo da requisição (POST)
             connection.setDoOutput(true);
 
-            //Seta na requisição a string json
+            //Pega o corpo da requisição na variavel output
             PrintStream output = new PrintStream(connection.getOutputStream());
+
+            //Seta na requisição os dados fornecidos como parâmetro em formato JSON
             output.println(json);
 
             //Abre a conexão
             connection.connect();
 
-            //Cria um scanner e retorna a resposta da requisição
+            //Cria um scanner e retorna a resposta da requisição enviada pelo servidor
             Scanner scanner = new Scanner(connection.getInputStream());
             return scanner.next();
 
